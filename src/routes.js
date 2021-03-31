1e8da325aba8c8b06d2877c324c6ae464b249ec7
Eugenio Barros <eugenio.barros.jr@gmail.com>
2021-03-31 15:30:17 -0300

    user create

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import Router from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (_, res) => {
-  const user = await User.create({
-    name: 'Eugênio Barros',
-    email: 'eugenio.barros.jr+outro@gmail.com',
-    password_hash: '12312sduiahj18asda',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
